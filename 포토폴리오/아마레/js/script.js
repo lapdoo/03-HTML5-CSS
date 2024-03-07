c9e581e02749e888afe09db09a107b5d11df33fe
lapdoo <wlxodua@gmail.com>
2024-03-07 18:01:00 +0900

    first commit

--- 포토폴리오/아마레/js/script.js
+++ 포토폴리오/아마레/js/script.js
@@ -23,7 +23,7 @@ var swiper2 = new Swiper(".mySwiper2", {
   effect: "fade",
   loop: true,
   autoplay: {
-    delay: 5000,
+    delay: 6000,
     pauseOnMouseEnter: true,
   },
   on: {
@@ -31,27 +31,21 @@ var swiper2 = new Swiper(".mySwiper2", {
       let i = this.activeIndex;
       console.log(i);
       $(".innerWrap .inner").eq(i).find(".rightImg").addClass("on");
-      $(".innerWrap .inner").eq(i).find("P").addClass("on");
     },
   },
 });
-$(".BottomImg > .btn_bottom > div:first-child").click(function () {
-  var $this = $(this);
-  var $slider = $this.closest(".BottomImg");
-  var $current = $slider.find(".page-nav > div.active");
-  var $post = $current.prev();
-  if ($post.length == 0) {
-    $post = $slider.find(".page-nav > div:last-child");
-  }
-  $post.click();
-});
-$(".slider-2 > .side-btns > div:last-child").click(function () {
-  var $this = $(this);
-  var $slider = $this.closest(".slider-2");
-  var $current = $slider.find(".page-nav > div.active");
-  var $post = $current.next();
-  if ($post.length == 0) {
-    $post = $slider.find(".page-nav > div:first-child");
-  }
-  $post.click();
+var swiper3 = new Swiper(".mySwiper3", {
+  // If we need pagination
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+  },
 });
